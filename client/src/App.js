71c9e003f9b3b3a316a0f7bab2b8efb707597f5c
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-06-27 11:56:32 -0700

    Display all items in client test

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';  // parses queries
+import { graphql } from 'react-apollo'; // for binding to component
+
+const TodosQuery = gql`
+  {
+    todos {
+      id
+      text
+      complete
+    }
+  }
+`;
 
 class App extends Component {
   render() {
+    const {data: { loading, todos }} = this.props;
+    if (loading) {
+      return null;
+    }
+
     return (
-      <div className="App">
-        hello graphql world!
+      <div>
+        {todos.map(todo => <div key={`${todo.id}-todo-item`}>{todo.text}</div>)}
       </div>
     );
   }
 }
 
-export default App;
+export default graphql(TodosQuery)(App);
